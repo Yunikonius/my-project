ca3ddebbc438c93c7b61f3f3788be15fbeb3b875
Yunikonius <Yunikonius@gmail.com>
2024-05-30 15:44:08 +0300

    update

--- task1.py
+++ task1.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 dir = '/Users/yurisafonov/Desktop/my_git_project/'
 organisations = pd.read_csv(dir + 'organisations.csv')
-features = pd.read_csv(dir  +  'features.csv')
-rubrics  = pd.read_csv(dir  +  'rubrics.csv')
 
-print(features.head())
+data = organisations.drop(columns=['org_id', 'city', 'rating', 'rubrics_id', 'features_id'], axis=1)
+grouped_data = data.groupby('average_bill').size()
+grouped_data = grouped_data.reset_index(name='count')
+plt.figure(figsize=(10,6))
+print(plt.hist(grouped_data['average_bill'].dropna(), bins=20, edgecolor='k', alpha=0.7))
