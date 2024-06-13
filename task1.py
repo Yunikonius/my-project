6eedde645579dfadb60f7c41bb58d8e641e2dcbc
Yunikonius <Yunikonius@gmail.com>
2024-06-13 14:14:18 +0300

    update

--- task1.py
+++ task1.py
@@ -5,7 +5,8 @@ dir = '/Users/yurisafonov/Desktop/my_git_project/'
 organisations = pd.read_csv(dir + 'organisations.csv')
 
 data = organisations.drop(columns=['org_id', 'city', 'rating', 'rubrics_id', 'features_id'], axis=1)
-grouped_data = data.groupby('average_bill').size()
+filtered_df = data.query('average_bill <= 2500 & average_bill.isna()')
+grouped_data = filtered_df.groupby('average_bill').size()
 grouped_data = grouped_data.reset_index(name='count')
 plt.figure(figsize=(10,6))
 print(plt.hist(grouped_data['average_bill'].dropna(), bins=20, edgecolor='k', alpha=0.7))
